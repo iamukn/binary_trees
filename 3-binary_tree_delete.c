8c38e2f56aaef653f6ca65c434d418728eb736bf
iamukn <n.u.kingsley@gmail.com>
2023-03-08 13:47:33 +0000

    deleting a bTree using recursion

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_delete - (binary_tree_t *tree) 
+ * binary_tree_delete - (binary_tree_t *tree)
  * @tree: The root tree
  *
  * Return Always 0
@@ -8,7 +8,7 @@
 void binary_tree_delete(binary_tree_t *tree)
 {
 if (tree == NULL)
-	return;
+return;
 binary_tree_delete(tree->left);
 binary_tree_delete(tree->right);
 free(tree);
