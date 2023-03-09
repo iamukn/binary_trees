24b3553cc1ba8a845616f7384ad0360be6ec7f31
iamukn <n.u.kingsley@gmail.com>
2023-03-09 16:38:53 +0000

    Fixed a bug on 5-binary_tree

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -9,11 +9,10 @@
 
 int binary_tree_is_root(const binary_tree_t *node)
 {
-binary_tree_t *root;
-root = node;
 
-if (root == NULL)
+if (node == NULL)
 	return (0);
-if (root->left != NULL || root->right != NULL)
+if (node->left != NULL || node->right != NULL)
 	return (1);
+return (0);
 }
