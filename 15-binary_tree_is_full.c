439a2db5cbd69f643f08989eba596772db98694a
iamukn <n.u.kingsley@gmail.com>
2023-03-09 21:50:36 +0000

    hot fix

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -8,13 +8,14 @@
 
 int is_full_recursive(const binary_tree_t *tree)
 {
-if (tree == NULL)
-	return (0);
+if (tree != NULL)
+{
 if ((tree->left != NULL && tree->right != NULL) ||
 (tree->left == NULL && tree->right == NULL) ||
 binary_tree_is_full(tree->left) == 0 ||
 binary_tree_is_full(tree->right) == 0)
 return (0);
+}
 return (1);
 }
 
