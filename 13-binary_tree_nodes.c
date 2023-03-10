6c3a9ec253bbb2870e7ff6716099e78665c824ba
iamukn <n.u.kingsley@gmail.com>
2023-03-10 17:57:02 +0000

    Counts the node with at least one child

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -12,9 +12,9 @@ if (tree == NULL)
 	return (0);
 if (tree)
 {
-count += tree->left || tree->right ? 1 : 0;
+count += tree->left != NULL || tree->right != NULL ? 1 : 0;
 count += binary_tree_nodes(tree->left);
 count += binary_tree_nodes(tree->right);
 }
-return (0);
+return (count);
 }
