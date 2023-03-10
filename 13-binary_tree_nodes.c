865234f5e06856120df772e1f3a8655debfa1d36
iamukn <n.u.kingsley@gmail.com>
2023-03-10 17:53:32 +0000

    Counts the node with at least one child

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_nodes - The prototype
+ * @tree: Pointer to the root node
+ * Return: Always 0
+ */
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+size_t count = 0;
+if (tree == NULL)
+	return (0);
+if (tree)
+{
+count += tree->left != NULL || tree->right != NULL ? 1 : 0;
+count += binary_tree_nodes(tree->left);
+count += binary_tree_nodes(tree->right);
+}
+return (0);
+}
